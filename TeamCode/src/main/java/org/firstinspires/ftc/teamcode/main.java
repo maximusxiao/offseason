7d657adee6a7e1403a598d32202791d2a8da9870
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-04 12:46:09 -0700

    Update main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -21,7 +21,7 @@ public class main extends LinearOpMode {
 
     //intake config
     private DcMotor extendo_motor;
-    private CRServo intake_servo;
+    private CRServo intake_crservo;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -30,7 +30,7 @@ public class main extends LinearOpMode {
         motor_fr = hardwareMap.dcMotor.get("motor_f_r");
         motor_br = hardwareMap.dcMotor.get("motor_b_r");
 
-        intake_servo = hardwareMap.crservo.get("servo_intake");
+        intake_crservo = hardwareMap.crservo.get("crservo_intake");
         extendo_motor = hardwareMap.dcMotor.get("motor_extendo");
 
     /*while (opModeInInit()) {
@@ -52,6 +52,6 @@ public class main extends LinearOpMode {
     public void update() {
         sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
         sys.extendo(gamepad2, extendo_motor);
-        sys.intake(gamepad2, intake_servo);
+        sys.intake(gamepad2, intake_crservo);
     }
 }
