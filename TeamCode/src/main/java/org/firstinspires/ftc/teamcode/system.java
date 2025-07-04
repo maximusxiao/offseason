07d18c32ed483dd5783410e57b19dd425df4186f
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-04 12:07:51 -0700

    Update and rename mecDrive.java to system.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system.java
@@ -6,14 +6,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-public class mecDrive {
+public class system {
     public void drive(Gamepad gamepad1, DcMotor motor_f_l, DcMotor motor_b_l, DcMotor motor_f_r, DcMotor motor_b_r) {
+
         // y_val reversed
         double y = -gamepad1.left_stick_y;
+
 
         // Multiplier of 1.17 to counteract strafing range
         double x = gamepad1.left_stick_x * 1.17;
         double rx = gamepad1.right_stick_x;
+
         // Range_limiter containes power within range [-1,1]
         double range_limiter = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double power_fl = -1 * ((y + x + rx) / range_limiter);
@@ -27,11 +30,26 @@ public class mecDrive {
         motor_f_r.setPower(power_fr);
         motor_b_r.setPower(power_br);
     }
-    public void extendo(Gamepad gamepad1, DcMotor horizontal_slides) {
-        horizontal_slides.setPower(gamepad1.left_trigger);
-        horizontal_slides.setPower(-gamepad1.right_trigger);
+    public void extendo(Gamepad gamepad2, DcMotor extendo) {
+        double extension = -gamepad2.right_stick_y;
+        double slide_power = extension * 0.5;
+
+        extendo.setPower(slide_power);
     }
 
-    //BETA!!!
+    public void intake(Gamepad gamepad2, CRServo intake_srvo) {
+        double fwd_spin = gamepad2.left_trigger;
+        double rvrse_spin = gamepad2.right_trigger;
+        double spin_power = fwd_spin - rvrse_spin;
+        
+        intake_srvo.setPower(spin_power);
+    }
+    
+    // BETA!!!
     // funny start up sequence cos why not
+    /*public void funny_start_sequence(DcMotor h_slide_motor) {
+        h_slide_motor.setPower(0.5);
+        Thread.sleep(750000000);
+        h_slide_motor.setPower(0.5;
+    }*/
 }
