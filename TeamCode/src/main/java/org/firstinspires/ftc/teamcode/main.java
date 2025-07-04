da9182ada8186d18b75f2404da7468946d05d932
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-04 12:07:20 -0700

    Update and rename solodrive.java to main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -4,12 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.robot;
 
 @TeleOp
-public class solodrive extends LinearOpMode {
+public class main extends LinearOpMode {
 
     // External class implementation to make code neater
-    private mecDrive sys = new mecDrive();
+    private robot sys = new robot();
 
     // Connect motors
     // ID should match config
@@ -20,6 +21,7 @@ public class solodrive extends LinearOpMode {
 
     //intake config
     private DcMotor extendo_motor;
+    private CRServo intake_servo;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -28,7 +30,8 @@ public class solodrive extends LinearOpMode {
         motor_fr = hardwareMap.dcMotor.get("motor_f_r");
         motor_br = hardwareMap.dcMotor.get("motor_b_r");
 
-        extendo_motor = hardwareMap.dcMotor.get("extendoMotor");
+        intake_servo = hardwareMap.crservo.get("servo_intake");
+        extendo_motor = hardwareMap.dcMotor.get("motor_extendo");
 
     /*while (opModeInInit()) {
             sys.funny_start_sequence(h_slide_motor);
@@ -49,5 +52,6 @@ public class solodrive extends LinearOpMode {
     public void update() {
         sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
         sys.extendo(gamepad2, extendo_motor);
+        sys.intake(gamepad2, intake_servo);
     }
 }
