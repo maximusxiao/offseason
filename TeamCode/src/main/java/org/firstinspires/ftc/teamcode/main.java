e843a71a9f933bb9118acb55c3fac51ec96e7275
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-06 10:29:10 -0700

    Update main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -60,9 +60,9 @@ public class main extends LinearOpMode {
         if (gamepad2.right_bumper) {
 	    sys.intake_pod(intake_pod_srvo);
 	}
-	/*if (gamepad2.left_bumper) {
+	if (gamepad2.left_bumper) {
 	    sys.pitch(pitch_servo);
-	}*/
+	}
 	sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
         sys.extendo(gamepad2, extendo);
         sys.intake(gamepad2, intake_crservo);
