2f0f262a86a04ef28cd0750e914f3c19b1df0cb4
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-05 21:46:14 -0700

    Update main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
@@ -15,12 +16,14 @@ public class main extends LinearOpMode {
     // Connect motors
     // ID should match config
     private DcMotorEx motor_fl, motor_bl, motor_fr, motor_br;
-    //intake config
+    
+	// Intake config
     private DcMotorEx extendo;
     private CRServo intake_crservo;
-
-    private DcMotorEx lift_l;
-    private DcMotorEx lift_r;
+	private Servo intake_pod_srvo, pitch_servo;
+	
+	// Lift
+    private DcMotorEx lift_l, lift_r;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -31,6 +34,8 @@ public class main extends LinearOpMode {
 
         intake_crservo = hardwareMap.get(CRServo.class, "crservo_intake");
         extendo = hardwareMap.get(DcMotorEx.class, "motor_extendo");
+		intake_pod_srvo = hardwareMap.get(Servo.class, "servo_intake_pod");
+		pitch_servo = hardwareMap.get(Servo.class, "servo_pitch");
 
         lift_l = hardwareMap.get(DcMotorEx.class, "motor_lift_l");
         lift_r = hardwareMap.get(DcMotorEx.class, "motor_lift_l");
@@ -52,7 +57,13 @@ public class main extends LinearOpMode {
         }
     }
     public void update() {
-        sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
+        if (gamepad2.right_bumper) {
+			sys.intake_pod(intake_pod_srvo);
+		}
+		if (gamepad2.left_bumper) {
+			sys.pitch(pitch_servo);
+		}
+		sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
         sys.extendo(gamepad2, extendo);
         sys.intake(gamepad2, intake_crservo);
         sys.lift(gamepad2, lift_l, lift_r);
