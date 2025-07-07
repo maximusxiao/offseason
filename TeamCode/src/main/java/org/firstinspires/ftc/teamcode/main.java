6b7ffb3dc142cc3c7bdd35c8ef036937ecc1b934
maximusxiao <xiaomaximus@gmail.com>
2025-07-06 18:35:38 -0700

    7/6 Sunday afternoon work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -3,12 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
 public class main extends LinearOpMode {
+    boolean forward=true;
+
 
     // External class implementation to make code neater
     private system sys = new system();
@@ -34,15 +37,17 @@ public class main extends LinearOpMode {
 
         intake_crservo = hardwareMap.get(CRServo.class, "crservo_intake");
         extendo = hardwareMap.get(DcMotorEx.class, "motor_extendo");
-	intake_pod_srvo = hardwareMap.get(Servo.class, "servo_intake_pod");
-	pitch_servo = hardwareMap.get(Servo.class, "servo_pitch");
+    	intake_pod_srvo = hardwareMap.get(Servo.class, "servo_intake_pod");
+	    pitch_servo = hardwareMap.get(Servo.class, "servo_pitch");
 
         lift_l = hardwareMap.get(DcMotorEx.class, "motor_lift_l");
         lift_r = hardwareMap.get(DcMotorEx.class, "motor_lift_l");
 
-    	/*while (opModeInInit()) {
-            sys.funny_start_sequence(h_slide_motor);
-        }*/
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad currentGamepad2 = new Gamepad();
+        Gamepad pastGamepad1 = new Gamepad();
+        Gamepad pastGamepad2 = new Gamepad();
+        String currIntakePitchPos;
 
         waitForStart();
 	
@@ -52,19 +57,18 @@ public class main extends LinearOpMode {
         }
         // Separate method implementation
         while (opModeIsActive()) {
-            this.update();
+            currIntakePitchPos = Double.toString(pitch_servo.getPosition());
+            telemetry.addData("intake pitch pos:", currIntakePitchPos);
+            telemetry.update();
+            pastGamepad2 = currentGamepad2;
+            if (gamepad2.left_bumper){
+                sys.pitch(pitch_servo);
+            }
+            sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
+            sys.extendo(gamepad2, extendo);
+            sys.intake(gamepad2, intake_crservo);
+            sys.lift(gamepad2, lift_l, lift_r);
+            currentGamepad2 = gamepad2;
         }
-    }
-    public void update() {
-        if (gamepad2.right_bumper) {
-	    sys.intake_pod(intake_pod_srvo);
-	}
-	if (gamepad2.left_bumper) {
-	    sys.pitch(pitch_servo);
-	}
-	sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
-        sys.extendo(gamepad2, extendo);
-        sys.intake(gamepad2, intake_crservo);
-        sys.lift(gamepad2, lift_l, lift_r);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system.java
@@ -1,20 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import java.util.concurrent.TimeUnit;
+
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class system {
     public void drive(Gamepad gamepad1, DcMotor motor_f_l, DcMotor motor_b_l, DcMotor motor_f_r, DcMotor motor_b_r) {
 
         // y_val reversed
-        double y = -gamepad1.left_stick_y;
+        double y = gamepad1.left_stick_y;
 
 
         // Multiplier of 1.17 to counteract strafing range
-        double x = gamepad1.left_stick_x * 1.17;
+        double x = -gamepad1.left_stick_x * 1.17;
         double rx = gamepad1.right_stick_x;
 
         // Range_limiter containes power within range [-1,1]
@@ -30,6 +38,7 @@ public class system {
         motor_f_r.setPower(power_fr);
         motor_b_r.setPower(power_br);
     }
+
     public void extendo(Gamepad gamepad2, DcMotorEx extendo) {
         double slide_power = -gamepad2.left_stick_y;
 
@@ -43,44 +52,35 @@ public class system {
 
         intake_crservo.setPower(spin_power);
     }
+
     public void lift(Gamepad gamepad2, DcMotorEx lift_l, DcMotorEx lift_r) {
         double lift_power = -gamepad2.right_stick_y;
-        
-	lift_l.setPower(lift_power);
-        lift_r.setPower(-lift_power);
-    }
-    public void intake_pod(Servo intake_pod) {
-        double curr_pos = intake_pod.getPosition();
-	double[] positions = [0.0, 0.33];
-	double next_pos;
-		
-	if (curr_pos == positions[0]) {
-	    next_pos = positions[1];
-	}
-	else {
-	    next_pos = positions[0];
-	}		
-	intake_pod.setPosition(next_pos)
-    }	
-    public void pitch(Servo pitch_servo) {
-	double curr_pitch = pitch_servo.getPosition();
-	double[] pitches = [0.0, 0.125];
-	double next_pitch;
-		
-	if (curr_pitch == pitches[0]) {
-	    next_pitch = pitches[1];
-	}
-	else {
-	    next_pitch = pitches[0];
-	}		
-	pitch_servo.setPosition(next_pitch)
+
+        lift_l.setPower(lift_power);
+        lift_r.setPower(lift_power);
     }
 
-    // BETA!!!
-    // funny start up sequence cos why not
-    /*public void funny_start_sequence(DcMotor h_slide_motor) {
-        h_slide_motor.setPower(0.5);
-        Thread.sleep(750000000);
-        h_slide_motor.setPower(0.5;
-    }*/
+    public void intake_pod(Servo intake_pod) {
+        double curr_pos = intake_pod.getPosition();
+        List<Double> positions = new ArrayList<>(Arrays.asList(0.0, 0.33));
+        double next_pos;
+
+
+        if (curr_pos == positions.get(0)) {
+            next_pos = positions.get(1);
+        } else {
+            next_pos = positions.get(0);
+        }
+        intake_pod.setPosition(next_pos);
+    }
+
+    public void pitch(Servo pitch_servo) {
+        /*if (curr_pos == 0.4) {
+            pitch_servo.setPosition(0.25);
+        } else if (curr_pos == 0.25) {
+            pitch_servo.setPosition(0.4);
+        } else if (curr_pos == 0.1) {
+            pitch_servo.setPosition(0.25);
+        }*/
+    }
 }
