58d2f4453ab2fe05d2569d09f8159873d7697265
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-06 22:01:14 -0700

    Update main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -3,15 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
 public class main extends LinearOpMode {
-    boolean forward=true;
-
 
     // External class implementation to make code neater
     private system sys = new system();
@@ -19,62 +16,75 @@ public class main extends LinearOpMode {
     // Connect motors
     // ID should match config
     private DcMotorEx motor_fl, motor_bl, motor_fr, motor_br;
-
-	// Intake config
+    
+    // Intake config
     private DcMotorEx extendo;
     private CRServo intake_crservo;
-    private Servo intake_arm, pitch_servo;
-
-	// Lift
+    private Servo intake_pod_srvo, pitch_servo;
+	
+    // Lift
     private DcMotorEx lift_l, lift_r;
+
+    // Transfer/Outtake
+    private Servo claw_servo, wrist_servo;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        motor_fl = hardwareMap.get(DcMotorEx.class, "motor_f_l");
+        // Edge detection
+	Gamepad curr_gpad1 = new Gamepad();
+        Gamepad curr_gpad2 = new Gamepad();
+        Gamepad past_gpad1 = new Gamepad();
+        Gamepad past_gpad2 = new Gamepad();
+	    
+	motor_fl = hardwareMap.get(DcMotorEx.class, "motor_f_l");
         motor_bl = hardwareMap.get(DcMotorEx.class, "motor_b_l");
         motor_fr = hardwareMap.get(DcMotorEx.class, "motor_f_r");
         motor_br = hardwareMap.get(DcMotorEx.class, "motor_b_r");
 
         intake_crservo = hardwareMap.get(CRServo.class, "crservo_intake");
         extendo = hardwareMap.get(DcMotorEx.class, "motor_extendo");
-    	intake_arm = hardwareMap.get(Servo.class, "servo_intake_pod");
-	    pitch_servo = hardwareMap.get(Servo.class, "servo_pitch");
+	intake_pod_srvo = hardwareMap.get(Servo.class, "servo_intake_pod");
+	pitch_servo = hardwareMap.get(Servo.class, "servo_pitch");
 
         lift_l = hardwareMap.get(DcMotorEx.class, "motor_lift_l");
         lift_r = hardwareMap.get(DcMotorEx.class, "motor_lift_l");
 
-        Gamepad currentGamepad1 = new Gamepad();
-        Gamepad currentGamepad2 = new Gamepad();
-        Gamepad pastGamepad1 = new Gamepad();
-        Gamepad pastGamepad2 = new Gamepad();
-        String currIntakePos;
+	claw_servo = hardwareMap.get(Servo.class, "servo_claw");
+	wrist_servo = hardwareMap.get(Servo.class, "servo_wrist");
 
-        intake_arm.setDirection(Servo.Direction.REVERSE);
-        intake_arm.setPosition(0.65);
+    	while (opModeInInit()) {
+            sys.funny_start_sequence(extendo);
+        }
 
         waitForStart();
-
+	
         // End when stop is pressed
         if (isStopRequested()) {
             return;
         }
         // Separate method implementation
         while (opModeIsActive()) {
-            currIntakePos = Double.toString(intake_arm.getPosition());
-            telemetry.addData("intake pitch pos:", currIntakePos);
-            telemetry.update();
-            pastGamepad1.copy(currentGamepad1);
-            pastGamepad2.copy(currentGamepad2);
-            currentGamepad1.copy(gamepad1);
-            currentGamepad2.copy(gamepad2);
-            if(currentGamepad2.left_bumper && !pastGamepad2.left_bumper){
-                sys.intake_arm(intake_arm);
-            }
-            sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
-            sys.extendo(gamepad2, extendo);
-            sys.intake(gamepad2, intake_crservo);
-            sys.lift(gamepad2, lift_l, lift_r);
-            sys.intake_pitch(pitch_servo);
+            this.update();
         }
+    }
+    public void update() {
+        past_gpad1.copy(currentGamepad1);
+        past_gpad2.copy(currentGamepad2);
+        curr_gpad1.copy(gamepad1);
+        curr_gpad2.copy(gamepad2);
+	    
+	if (curr_gpad2.right_bumper && !past_gpad2.right_bumper) {
+	    sys.intake_pod(intake_pod_srvo);
+	}
+	if (curr_gpad2.left_bumper && !past_gpad2.left_bumper) {
+	    sys.pitch(pitch_servo);
+	}
+	if (curr_gpad2.a && !past_gpad2.a) {
+	    sys.claw(claw_servo);
+	}
+	sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
+        sys.extendo(gamepad2, extendo);
+        sys.intake(gamepad2, intake_crservo);
+        sys.lift(gamepad2, lift_l, lift_r);
     }
 }
