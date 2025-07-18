dbc2dc57271c9750f9dcfaecb3258f48c148bc3e
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-17 20:59:17 -0700

    Update command_tele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command_tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command_tele.java
@@ -4,17 +4,26 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.rowanmcalpin.nextftc.core.command.Command;
 import com.rowanmcalpin.nextftc.core.command.groups.ParallelGroup;
 import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
+import com.rowanmcalpin.nextftc.core.command.utility.delays.Delay;
 import com.rowanmcalpin.nextftc.ftc.driving.MecanumDriverControlled;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
 import com.rowanmcalpin.nextftc.pedro.PedroOpMode;
 
+import org.firstinspires.ftc.teamcode.Subsystems.claw;
 import org.firstinspires.ftc.teamcode.Subsystems.extendo;
+import org.firstinspires.ftc.teamcode.Subsystems.intake;
+import org.firstinspires.ftc.teamcode.Subsystems.intake_arm;
 import org.firstinspires.ftc.teamcode.Subsystems.lift;
+import org.firstinspires.ftc.teamcode.Subsystems.lift_arm;
+import org.firstinspires.ftc.teamcode.Subsystems.pitch;
+import org.firstinspires.ftc.teamcode.Subsystems.pto;
+import org.firstinspires.ftc.teamcode.Subsystems.stilts;
+import org.firstinspires.ftc.teamcode.Subsystems.wrist;
 
 @TeleOp(name = "Command OpMode")
 public class command_tele extends PedroOpMode {
     public command_tele() {
-        super(lift.INSTANCE, extendo.INSTANCE);
+        super(lift.INSTANCE, extendo.INSTANCE, claw.INSTANCE, pitch.INSTANCE, intake_arm.INSTANCE, lift_arm.INSTANCE, pto.INSTANCE, stilts.INSTANCE, wrist.INSTANCE, intake.INSTANCE);
     }
 
     // Hardware declaration
@@ -48,8 +57,75 @@ public class command_tele extends PedroOpMode {
         manual_drive.invoke();
         gamepadManager.getGamepad2().getLeftStick().setDisplacedCommand((value) -> lift.INSTANCE.lift_power(value.component2()));
         gamepadManager.getGamepad2().getRightStick().setDisplacedCommand((value) -> extendo.INSTANCE.extension_power(value.component2()));
+
+        gamepadManager.getGamepad2().getRightBumper().setPressedCommand(
+                () -> new SequentialGroup(
+                        new ParallelGroup(
+                                intake_arm.INSTANCE.transfer(),
+                                pitch.INSTANCE.reject(),
+                                intake.INSTANCE.intake(0.35)
+                        ),
+                        new Delay(0.075),
+                        new ParallelGroup(
+                                claw.INSTANCE.open(),
+                                wrist.INSTANCE.intake(),
+                                lift_arm.INSTANCE.sample_intake()
+                        ),
+                        new Delay(0.15),
+                        new ParallelGroup(
+                                claw.INSTANCE.close(),
+                                lift_arm.INSTANCE.sample_intake(),
+                                wrist.INSTANCE.spec()
+                        )
+                )
+        );
+        gamepadManager.getGamepad2().getRightTrigger().setStateChangeCommand(
+                value -> new ParallelGroup(
+                        intake_arm.INSTANCE.intake(),
+                        pitch.INSTANCE.intake(),
+                        intake.INSTANCE.intake(value)
+                )
+        );
+        gamepadManager.getGamepad2().getLeftTrigger().setStateChangeCommand(
+                value -> new ParallelGroup(
+                        intake_arm.INSTANCE.intake(),
+                        pitch.INSTANCE.reject(),
+                        intake.INSTANCE.outtake(value)
+                )
+        );
+        gamepadManager.getGamepad2().getDpadUp().setPressedCommand(
+                () -> new SequentialGroup(
+                        stilts.INSTANCE.raise(),
+                        lift.INSTANCE.lift_power(1),
+                        new Delay(0.75),
+                        pto.INSTANCE.engage()
+                )
+        );
+        gamepadManager.getGamepad2().getA().setPressedCommand(
+                () -> new SequentialGroup(
+                        new ParallelGroup(
+                                lift_arm.INSTANCE.spec_intake(),
+                                claw.INSTANCE.open()
+                        ),
+                        new Delay(0.3),
+                        claw.INSTANCE.close(),
+                        lift_arm.INSTANCE.spec_dep()
+                )
+        );
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
